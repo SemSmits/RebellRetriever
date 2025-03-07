0774c0b031ab140729694a85898a40ebfdff21ff
Sem <sem.smits@student.hu.nl>
2025-03-07 14:56:27 +0100

    Project setup

---
+++ src/main/java/com/example/rebellretriever/RebellRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebellretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebellRetrieverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebellRetrieverApplication.class, args);
+    }
+
+}
